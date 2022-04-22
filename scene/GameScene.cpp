89e8063dd7c2ad4c22612b28706f2d7e8d76783f
Shizuku_Kazuki <k021g1377@g.neec.ac.jp>
2022-04-22 14:56:08 +0900

    02_02_プレイヤー

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -7,12 +7,16 @@ using namespace DirectX;
 GameScene::GameScene() {}
 //デストラクタ
 GameScene::~GameScene() {
-	delete sprite_;
-	delete model_;
+	//delete sprite_;
+	//delete model_;
+
+	delete spriteBG_; //GB
+	delete modelStage_; //ステージ
+	delete modelPlayer_; //プレイヤー
 }
 
 
-
+//初期化
 void GameScene::Initialize() {
 
 	dxCommon_ = DirectXCommon::GetInstance();
@@ -22,48 +26,81 @@ void GameScene::Initialize() {
 
 	
 
-	textureHandle_ = TextureManager ::Load("pic.png");
+	//textureHandle_ = TextureManager ::Load("pic.png");
 
 	//生成
-	sprite_ = Sprite ::Create(textureHandle_, {100, 50});
+	//sprite_ = Sprite ::Create(textureHandle_, {100, 50});
 	//音声
-	soundDataHandle_ = audio_->LoadWave("mokugyo.wav");
+	//soundDataHandle_ = audio_->LoadWave("mokugyo.wav");
 
-	audio_->PlayWave(soundDataHandle_);
+	//audio_->PlayWave(soundDataHandle_);
 
 	//3D生成
-	model_ = Model ::Create();
+	//model_ = Model ::Create();
 	//ワールドトンランスフォーム初期化
 	ｗorldTransform_.Initialize();
 	//ビュープロジェクション初期化
 	viewProjection_.Initialize();
 
 	//音声再生
-	voiceHandle_ = audio_->PlayWave(soundDataHandle_, true);
-}
+	//voiceHandle_ = audio_->PlayWave(soundDataHandle_, true);
+	// 
+	//＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊
+	//背景　ステージ
+	//BG(2Dスプライト)背景
+	textureHandleBG_ = TextureManager ::Load("bg.jpg");
+	spriteBG_ = Sprite ::Create(textureHandleBG_, {0,0,});
 
+	//ビュープロジェクションの初期化
+	viewProjection_ .Initialize();
+	//ステージ表示(拡大　移動)
+	viewProjection_.eye = {0, 1, -6};
+	viewProjection_.target = {0, 1, 0};
+	viewProjection_.Initialize();
+
+	modelStage_ = Model ::Create();
+	worldTransformStage_.translation_ = {0, -1.5f, 0};
+	worldTransformStage_.scale_ = {4.5f, 1, 40};
+	worldTransformStage_.Initialize();
+
+	//ステージ
+	textureHandleStage_ = TextureManager ::Load("stage.jpg");
+	modelStage_ = Model ::Create();
+	worldTransformStage_.Initialize();
+	//＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊
+
+	//プレイヤー
+	textureHandlePlayer_ = TextureManager ::Load("player.png");
+	modelPlayer_ = Model ::Create();
+	worldTransformPlayer_.scale_ = {0.5f, 0.5f, 0.5f};
+	worldTransformPlayer_.Initialize();
+}
+//更新
 void GameScene::Update()  
 {
-	if (input_->TriggerKey(DIK_SPACE)) {
+	/*if (input_->TriggerKey(DIK_SPACE)) {
 		audio_->StopWave(voiceHandle_);
-	}
+	}*/
 
 	value_++;
 
-	std ::string strDebug = std::string("value") +
-	std ::to_string(value_);
+	/*std ::string strDebug = std::string("value") +
+	std ::to_string(value_);*/
+	//debugText_->Print(strDebug, 50, 50, 1.0f);
 
-	debugText_->Print(strDebug, 50, 50, 1.0f);
+
+	PlayerUpdate(); //プレイヤー更新
 }
-
+//表示
 	void GameScene::Draw() {
 
-	XMFLOAT2 position = sprite_->GetPosition();
 
-	position.x += 2.0f;
+	//XMFLOAT2 position = sprite_->GetPosition();
+
+	/*position.x += 2.0f;
 	position.y += 1.0f;
 
-	sprite_->SetPosition(position);
+	sprite_->SetPosition(position);*/
 	// コマンドリストの取得
 	ID3D12GraphicsCommandList* commandList = dxCommon_->GetCommandList();
 
@@ -73,7 +110,9 @@ void GameScene::Update()
 
 	/// <summary>
 	/// ここに背景スプライトの描画処理を追加できる
-	
+	//背景
+	spriteBG_->Draw();
+
 	/// </summary>
 
 	// スプライト描画後処理
@@ -85,11 +124,14 @@ void GameScene::Update()
 #pragma region 3Dオブジェクト描画
 	// 3Dオブジェクト描画前処理
 	Model::PreDraw(commandList);
-
+	//ステージ
+	modelStage_->Draw(worldTransformStage_, viewProjection_, textureHandleStage_);
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
-	model_->Draw(ｗorldTransform_, viewProjection_, textureHandle_);
+	//model_->Draw(ｗorldTransform_, viewProjection_, textureHandle_);
+	//プレイヤー
+	modelPlayer_->Draw(worldTransformPlayer_, viewProjection_, textureHandlePlayer_);
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
     #pragma endregion
@@ -102,7 +144,7 @@ void GameScene::Update()
 	/// ここに前景スプライトの描画処理を追加できる
 	
 	/// </summary>
-	sprite_ ->Draw();
+	//sprite_ ->Draw();
 	// デバッグテキストの描画
 	debugText_->DrawAll(commandList);
 	//
@@ -110,4 +152,27 @@ void GameScene::Update()
 	Sprite::PostDraw();
 	
 #pragma endregion
-}
+    }
+
+	//＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊
+	//プレイヤーの更新
+    void GameScene::PlayerUpdate()
+	{
+		//移動
+		//右移動
+	    if (input_->PushKey(DIK_RIGHT))
+		{
+		    worldTransformPlayer_.translation_.x += 0.1f;	    
+		}
+
+		//左移動
+	    if (input_->PushKey(DIK_LEFT))
+		{
+		    worldTransformPlayer_.translation_.x -= 0.1f;	
+		}
+
+		//行列更新
+	    worldTransformPlayer_.UpdateMatrix();
+
+	}
+
--- scene/GameScene.h
+++ scene/GameScene.h
@@ -53,26 +53,51 @@ class GameScene {
 	/// </summary>
 	
 	//テクスチャハンドル
-	uint32_t textureHandle_ = 0;
+	//uint32_t textureHandle_ = 0;
 
 	//スプライト
-	Sprite* sprite_ = nullptr;
+	//Sprite* sprite_ = nullptr;
 
 	//サウンドデータ
-	uint32_t soundDataHandle_ = 0;
+	//uint32_t soundDataHandle_ = 0;
 
     //3D
-	Model* model_ = nullptr;
+	//Model* model_ = nullptr;
+
+	
+
+	//音声再生ハンドル
+	//uint32_t voiceHandle_ = 0;
+
+	//値を表示
+	int32_t value_ = 0;
+	//********************************************************************
+    //背景の表示
+
+	//BG(スプライト)
+	uint32_t textureHandleBG_ = 0;
+	Sprite* spriteBG_ = nullptr;
+
+	//************************************
+	// ステージ表示
 
 	//ワールドトランス
 	WorldTransform ｗorldTransform_;
 	//ビュープロジョエルロブション
 	ViewProjection viewProjection_;
 
-	//音声再生ハンドル
-	uint32_t voiceHandle_ = 0;
+	//ステージ
+	uint32_t textureHandleStage_ = 0;
+	Model* modelStage_ = nullptr;
+	WorldTransform worldTransformStage_;
 
-	//値を表示
-	int32_t value_ = 0;
+	//＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊
+	//プレイヤーの表示
+	uint32_t textureHandlePlayer_ = 0;
+	Model* modelPlayer_ = nullptr;
+	WorldTransform worldTransformPlayer_;
+
+	void PlayerUpdate(); //プレイヤー更新
+	
 
 };
